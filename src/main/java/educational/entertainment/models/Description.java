8702f0a2d6b3711a0972092c7988ddf4ce9c816e
Ziad Aamer <ziadaamer9@gmail.com>
2017-05-14 00:55:13 +0200

    8

---
+++ src/main/java/educational/entertainment/models/Description.java
@@ -0,0 +1,19 @@
+﻿package educational.entertainment.models;
+
+public class Description {
+	
+	String des;
+	int ID;
+	public String getDes() {
+		return des;
+	}
+	public void setDes(String des) {
+		this.des = des;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+}
